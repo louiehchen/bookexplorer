f4fb308e9e990b89730aeef6afc128820199ceb5
Louie <louiehchen@gmail.com>
2017-05-08 20:26:24 -0700

    Add in gallery and correct API calls to book covers and info, books now link to their info page on google play store, update readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Simple React App Book Explorer to learn React.js and ES6.
+This app calls the Google Books API to get info about books that users can search for.
---
+++ app/components/gallery.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Gallery extends Component {
+  render() {
+    let alternate = './assets/book.icns'
+    return (
+      <div>
+        {
+          this.props.items.map((item, index) => {
+            let {title, imageLinks, infoLink} = item.volumeInfo;
+            return (
+              <a
+                key={index}
+                className="book"
+                href={infoLink}
+                target="_blank"
+              >
+                <img
+                  src={imageLinks !== undefined? imageLinks.thumbnail : ''}
+                  alt="book image" className="book-img"/>
+                <div className="book-text">
+                  {title}
+                </div>
+
+              </a>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default Gallery;
